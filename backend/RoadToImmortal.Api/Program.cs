7e4472e306a886e2168b6352189eb896a6153891
srmacpherson <srmacpherson96@gmail.com>
2026-09-23 20:41:09 +0100

    remove OpenAPI dummy code

--- backend/RoadToImmortal.Api/Program.cs
+++ backend/RoadToImmortal.Api/Program.cs
@@ -24,10 +24,6 @@ if (app.Environment.IsDevelopment())
 
 //app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
 app.MapGet("/", () =>
 {
@@ -62,20 +58,6 @@ app.MapGet("/players/{steamId}/matches", async (long steamId, DotaService dotaSe
     return Results.Ok(matches);
 });
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapPost("/players", async (CreatePlayerRequest request, AppDbContext db) =>
 {
     var player = new Player
@@ -99,8 +81,3 @@ record CreatePlayerRequest(
     string PersonaName,
     int? CurrentMmr
 );
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
